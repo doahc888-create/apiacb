95e3ff577ac829da96c488fb3dbce745afcc36e5
doahc888-create <doahc888@gmail.com>
2025-10-18 01:41:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import express from "express";
-import cors from "cors";
-
 const app = express();
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running ✅");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
 app.get("/test", (req, res) => {
   res.json({ message: "API hoạt động ngon rồi 😎" });
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
